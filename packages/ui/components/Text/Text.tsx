69a8013e3a0240a5d23efd863121dc2b7c844e18
Henrique Caiano <henriqueccaiano@gmail.com>
2022-04-02 16:13:31 +0100

    refactor(Text): wrap text with a forwardRef

--- packages/ui/components/Text/Text.tsx
+++ packages/ui/components/Text/Text.tsx
@@ -1,12 +1,21 @@
 import { TextProps } from './types';
 
 import * as S from './styles';
+import { forwardRef, ForwardRefRenderFunction } from 'react';
 
-const Text = (props: TextProps) => {
+type TextWithRefType = ForwardRefRenderFunction<HTMLElement, TextProps>;
+
+const TextElement: TextWithRefType = (props, ref) => {
   const { children, ...rest } = props;
 
-  return <S.Text>{children}</S.Text>;
+  return (
+    <S.Text ref={ref} {...rest}>
+      {children}
+    </S.Text>
+  );
 };
+
+const Text = forwardRef(TextElement);
 
 Text.defaultProps = {
   textTransform: 'none',
