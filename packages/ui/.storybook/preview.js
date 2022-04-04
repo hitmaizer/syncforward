b8b1ef5290b038f1cf58405c28438b3ad7229ad1
hitmaizer <jose.sousalves@gmail.com>
2022-04-04 16:40:22 +0100

    feat(storybook): decorators for stories
    imported theme provider + global styles for stories preview

--- packages/ui/.storybook/preview.js
+++ packages/ui/.storybook/preview.js
@@ -1,9 +1,22 @@
+import { ThemeProvider } from 'styled-components';
+import GlobalStyles from '@styles/globalStyles';
+import theme from '@styles/theme/base';
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Story />
+    </ThemeProvider>
+  ),
+];
