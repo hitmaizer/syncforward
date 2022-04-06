f9fd2d9829906c2007a01df622c489319496186c
hitmaizer <jose.sousalves@gmail.com>
2022-04-06 21:14:24 +0100

    feat(box component): box component markup styles and types

---
+++ packages/ui/components/Box/Box.tsx
@@ -0,0 +1,29 @@
+import { forwardRef, ForwardRefRenderFunction } from 'react';
+import styled from 'styled-components';
+import { layout, flexbox, space, color, position } from 'styled-system';
+import { BoxProps } from './types';
+
+const StyledBox = styled.div`
+  ${layout}
+  ${flexbox}
+  ${space}
+  ${color}
+  ${position}
+`;
+
+const BoxElement: ForwardRefRenderFunction<HTMLDivElement, BoxProps> = (
+  props,
+  ref
+) => {
+  const { children, ...rest } = props;
+
+  return (
+    <StyledBox ref={ref} {...rest}>
+      {children}
+    </StyledBox>
+  );
+};
+
+const Box = forwardRef(BoxElement);
+
+export default Box;
