302cc519acc0a0ed282e0b647ab85131a34c9769
Jose Alves <jose.sousalves@gmail.com>
2022-04-10 19:57:21 +0100

    feat(nextapp): add web nextjs app

---
+++ apps/web/pages/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next';
+import { Box, Grid, Text } from 'ui';
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <h1>Hello mate</h1>
+      <Text>Hello mate!</Text>
+      <Grid>Estes devs nao brincam!</Grid>
+      <Box></Box>
+    </div>
+  );
+};
+
+export default Home;
