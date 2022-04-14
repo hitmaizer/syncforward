35e179493e38a4524fabf1c49ff43ac4dc15b7cf
Jose Alves <jose.sousalves@gmail.com>
2022-04-14 18:36:01 +0100

    refactor(MusicCard): refactor from react-player to iframe

--- packages/ui/components/MusicCard/MusicCard.tsx
+++ packages/ui/components/MusicCard/MusicCard.tsx
@@ -1,7 +1,5 @@
 import { useState } from 'react';
 
-import SoundCloudPlayer from 'react-player/soundcloud';
-
 import * as S from './MusicCard.styles';
 import { MusicCardProps } from './MusicCard.types';
 
@@ -11,12 +9,17 @@ const MusicCard = ({ children, imgSrc }: MusicCardProps) => {
   return (
     <>
       <S.SoundCloudContainer>
-        <SoundCloudPlayer
-          allow="autoplay"
-          width="500px"
-          height="500px"
-          url="https://soundcloud.com/syncforward/remotion-multiverse-original-mix?in=syncforward/sets/sequence-four-va&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing"
-        />
+        {isClicked && (
+          <iframe
+            title="sc-player"
+            width="500px"
+            height="500px"
+            scrolling="no"
+            frameBorder="no"
+            allow="autoplay"
+            src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1209898735&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
+          />
+        )}
       </S.SoundCloudContainer>
       <S.Card onClick={() => setIsClicked(!isClicked)} isClicked={isClicked}>
         <img src={imgSrc} alt="card-img" />
