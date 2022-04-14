da90f2278d453763359115d5e3f1abd823d56c53
Jose Alves <jose.sousalves@gmail.com>
2022-04-14 17:03:23 +0100

    feat(MusicCard): add react-player and state to component

--- packages/ui/components/MusicCard/MusicCard.tsx
+++ packages/ui/components/MusicCard/MusicCard.tsx
@@ -1,13 +1,28 @@
-/* eslint-disable react/style-prop-object */
+import { useState } from 'react';
+
+import SoundCloudPlayer from 'react-player/soundcloud';
+
 import * as S from './MusicCard.styles';
 import { MusicCardProps } from './MusicCard.types';
 
 const MusicCard = ({ children, imgSrc }: MusicCardProps) => {
+  const [isClicked, setIsClicked] = useState(false);
+
   return (
-    <S.Card>
-      <img src={imgSrc} alt="card-img" />
-      {children}
-    </S.Card>
+    <>
+      <S.SoundCloudContainer>
+        <SoundCloudPlayer
+          allow="autoplay"
+          width="500px"
+          height="500px"
+          url="https://soundcloud.com/syncforward/remotion-multiverse-original-mix?in=syncforward/sets/sequence-four-va&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing"
+        />
+      </S.SoundCloudContainer>
+      <S.Card onClick={() => setIsClicked(!isClicked)} isClicked={isClicked}>
+        <img src={imgSrc} alt="card-img" />
+        {children}
+      </S.Card>
+    </>
   );
 };
 
