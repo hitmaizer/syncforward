c08e7cfd1e6b866f1fa8047a0447ffdd134877d0
Jose Alves <jose.sousalves@gmail.com>
2022-04-14 22:07:15 +0100

    feat(MusicCard): soundcloudLink and title from props

--- packages/ui/components/MusicCard/MusicCard.tsx
+++ packages/ui/components/MusicCard/MusicCard.tsx
@@ -3,25 +3,37 @@ import { useState } from 'react';
 import * as S from './MusicCard.styles';
 import { MusicCardProps } from './MusicCard.types';
 
-const MusicCard = ({ children, imgSrc }: MusicCardProps) => {
+const MusicCard = ({
+  children,
+  imgSrc,
+  soundcloudLink = 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1209898735&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true',
+  title,
+}: MusicCardProps) => {
   const [isClicked, setIsClicked] = useState(false);
+
+  const handlePlay = () => {
+    setIsClicked(!isClicked);
+    document.querySelectorAll('iframe').forEach((elem: any) => {
+      return elem.parentNode.removeChild(elem);
+    });
+  };
 
   return (
     <>
       <S.SoundCloudContainer>
         {isClicked && (
           <iframe
-            title="sc-player"
+            title={title}
             width="500px"
             height="500px"
             scrolling="no"
             frameBorder="no"
             allow="autoplay"
-            src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1209898735&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
+            src={soundcloudLink}
           />
         )}
       </S.SoundCloudContainer>
-      <S.Card onClick={() => setIsClicked(!isClicked)} isClicked={isClicked}>
+      <S.Card onClick={handlePlay} isClicked={isClicked}>
         <img src={imgSrc} alt="card-img" />
         {children}
       </S.Card>
