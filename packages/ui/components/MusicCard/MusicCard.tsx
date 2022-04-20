6d165b93a52792c4386bfbaee3786510308c5d36
Jose Alves <jose.sousalves@gmail.com>
2022-04-20 19:37:11 +0100

    feat(MusicCard): Logic for MusicCard component start

--- packages/ui/components/MusicCard/MusicCard.tsx
+++ packages/ui/components/MusicCard/MusicCard.tsx
@@ -1,41 +1,47 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import * as S from './MusicCard.styles';
 import { MusicCardProps } from './MusicCard.types';
 
 const MusicCard = ({
-  children,
   imgSrc,
   soundcloudLink,
   title,
+  iconSrc,
 }: MusicCardProps) => {
-  const [isClicked, setIsClicked] = useState(false);
+  const [showPlay, setShowPlay] = useState(true);
+  const [soundcloudLinkState, setSoundcloudLinkState] = useState('');
 
-  const handlePlay = () => {
-    setIsClicked(!isClicked);
-    document.querySelectorAll('iframe').forEach((elem: any) => {
-      return elem.parentNode.removeChild(elem);
-    });
+  useEffect(() => {
+    setShowPlay(false);
+  }, [soundcloudLinkState]);
+
+  const handlePlay = (link: string) => {
+    setShowPlay(!showPlay);
+    setSoundcloudLinkState(link!);
   };
 
   return (
     <>
-      <S.SoundCloudContainer>
-        {isClicked && (
-          <iframe
-            title={title}
-            width="500px"
-            height="500px"
-            scrolling="no"
-            frameBorder="no"
-            allow="autoplay"
-            src={soundcloudLink}
-          />
+      <S.Card onClick={() => handlePlay(soundcloudLink!)} showPlay={showPlay}>
+        {soundcloudLinkState && (
+          <S.SoundCloudContainer showPlay={showPlay}>
+            <iframe
+              title={title}
+              width="500px"
+              height="500px"
+              scrolling="no"
+              frameBorder="no"
+              allow="autoplay"
+              src={soundcloudLinkState}
+            />
+          </S.SoundCloudContainer>
         )}
-      </S.SoundCloudContainer>
-      <S.Card onClick={handlePlay} isClicked={isClicked}>
+
         <img src={imgSrc} alt="card-img" />
-        {children}
+        <S.OpenSoundCloud>
+          <S.Icon src={iconSrc} alt="" />
+        </S.OpenSoundCloud>
       </S.Card>
     </>
   );
@@ -47,4 +53,5 @@ MusicCard.defaultProps = {
   imgSrc: 'assets/imgs/placeholder.jpg',
   soundcloudLink:
     'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1209898735&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true',
+  iconSrc: 'assets/imgs/bx-play-circle.svg',
 };
