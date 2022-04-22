aadeb418face7b9d18dcd15a92afd81f394e7f8f
Jose Alves <jose.sousalves@gmail.com>
2022-04-22 22:28:45 +0100

    style(Stack): getting place-content style rule from StackProps

--- packages/ui/components/Stack/Stack.styles.ts
+++ packages/ui/components/Stack/Stack.styles.ts
@@ -5,6 +5,7 @@ import { Box } from '..';
 import { StackProps } from './Stack.types';
 
 export const Stack = styled(Box)<StackProps>`
+  place-content: ${({ placeContent }) => placeContent};
   flex-direction: ${(props) => (props.vertical ? 'column' : 'row')};
   ${gridGap}
 `;
