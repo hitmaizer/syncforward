dfa3c8020ad7eab25ac72d2c1c55ada82139c4c5
Jose Alves <jose.sousalves@gmail.com>
2022-04-22 22:34:48 +0100

    feat(Stack): place-items style rule from StackProps

--- packages/ui/components/Stack/Stack.styles.ts
+++ packages/ui/components/Stack/Stack.styles.ts
@@ -6,6 +6,7 @@ import { StackProps } from './Stack.types';
 
 export const Stack = styled(Box)<StackProps>`
   place-content: ${({ placeContent }) => placeContent};
+  place-items: ${({ placeItems }) => placeItems};
   flex-direction: ${(props) => (props.vertical ? 'column' : 'row')};
   ${gridGap}
 `;
