45bdde3010565f3c83fe4d7b7ed6cffe038db5e6
Jose Alves <jose.sousalves@gmail.com>
2022-05-18 10:10:03 +0100

    feat(cms): findOne method searching release by slug

--- apps/cms/src/api/release/controllers/release.js
+++ apps/cms/src/api/release/controllers/release.js
@@ -1,9 +1,18 @@
-'use strict';
-
 /**
  *  release controller
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::release.release');
+module.exports = createCoreController('api::release.release', ({ strapi }) => ({
+  async findOne(ctx) {
+    const { id: slug } = ctx.params;
+    const { query } = ctx;
+    if (!query.filters) query.filters = {};
+    query.filters.slug = { $eq: slug };
+    const entity = await strapi.service('api::release.release').find(query);
+    const { results } = await this.sanitizeOutput(entity, ctx);
+
+    return this.transformResponse(results[0]);
+  },
+}));
