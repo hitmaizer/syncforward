c481b9578c3b8aaff8f83b3a0371d1b2f127ccc7
Jose Alves <jose.sousalves@gmail.com>
2022-05-18 10:09:10 +0100

    feat(cms): findOne method searching genre by slug

--- apps/cms/src/api/genre/controllers/genre.js
+++ apps/cms/src/api/genre/controllers/genre.js
@@ -1,9 +1,18 @@
-'use strict';
-
 /**
  *  genre controller
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::genre.genre');
+module.exports = createCoreController('api::genre.genre', ({ strapi }) => ({
+  async findOne(ctx) {
+    const { id: slug } = ctx.params;
+    const { query } = ctx;
+    if (!query.filters) query.filters = {};
+    query.filters.slug = { $eq: slug };
+    const entity = await strapi.service('api::genre.genre').find(query);
+    const { results } = await this.sanitizeOutput(entity, ctx);
+
+    return this.transformResponse(results[0]);
+  },
+}));
