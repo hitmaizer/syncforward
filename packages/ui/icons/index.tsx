2ccc749e07118ea10b9ba991b2d69f6144e7a78c
Jose Alves <jose.sousalves@gmail.com>
2022-06-24 15:50:25 +0100

    feat(icons): adds converted Components from svg icons

--- packages/ui/icons/index.tsx
+++ packages/ui/icons/index.tsx
@@ -1,11 +1,25 @@
 import { SpaceProps, ColorProps } from 'styled-system';
 
+ import SfAppleMusic from './AppleMusic';
+import SfBandcamp from './Bandcamp';
+import SfBeatport from './Beatport';
+import SfSoundcloud from './Soundcloud';
+import SfSpotify from './Spotify';
 import SfSync from './Sync';
+import SfYoutube from './Youtube';
 
-interface BaseProps {
-  size?: 'xxs' | 'xs' | 'sm' | 'md' | 'lg' | 'xl';
-}
+  interface BaseProps {
+    size?: 'xxs' | 'xs' | 'sm' | 'md' | 'lg';
+  }
 
-export type IconProps = BaseProps & SpaceProps & ColorProps;
+  export type IconProps = BaseProps & SpaceProps & ColorProps;
 
-export { SfSync };
+ export {
+  SfAppleMusic,
+  SfBandcamp,
+  SfBeatport,
+  SfSoundcloud,
+  SfSpotify,
+  SfSync,
+  SfYoutube
+ };
