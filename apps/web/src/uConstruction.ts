216e675db1a3575e1759320b55b8a6740671a8f6
Jose Alves <jose.sousalves@gmail.com>
2022-06-24 15:56:59 +0100

    feat(uConstruction): adds obj with random sentences to render

---
+++ apps/web/src/uConstruction.ts
@@ -0,0 +1,15 @@
+export const phrases: string[] = [
+  "We're building something new",
+  'be right back',
+  "hey there, how's it going?",
+  'under construction',
+  "we'll be right back",
+  'we are working on it',
+  'loading, please wait',
+  "you're still here?",
+  'this will take a while',
+  'just grab a magazine or something',
+  "we'll be with you shortly",
+  'aaany minute now',
+  'almost there...',
+];
