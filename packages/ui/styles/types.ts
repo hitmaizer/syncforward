df5dd1b6eaae1fde4cb42f8d02a5683d48dccce6
Jose Alves <jose.sousalves@gmail.com>
2022-06-24 16:00:05 +0100

    refactor(theme ): removes unused types from theme types file

--- packages/ui/styles/types.ts
+++ packages/ui/styles/types.ts
@@ -13,77 +13,6 @@ export type ColorScheme =
   | 'warning'
   | 'contrast';
 
-type ColorVariant = {
-  main: string;
-  text: string;
-  bg: string;
-  alpha: string;
-};
-
-type AdditionalColors = {
-  white: string;
-  black: string;
-  inverse: string;
-  border: string;
-  text: {
-    primary: string;
-    secondary: string;
-    muted: string;
-    transparent: string;
-  };
-  background: {
-    main: string;
-    secondary: string;
-    gradient: string;
-    fadeGradient: string;
-  };
-  appGradient: {
-    left: string;
-    right: string;
-  };
-  surface: {
-    main: string;
-    secondary: string;
-    gradient: string;
-  };
-  input: {
-    focus: {
-      boxShadow: string;
-      border: string;
-    };
-  };
-  glow: {
-    text: {
-      primary: string;
-      contrast: string;
-    };
-    card: {
-      warning: string;
-    };
-  };
-  shadow: {
-    normal: string;
-    raised: string;
-    small: string;
-  };
-  dropdown: {
-    border: string;
-    shadow: string;
-  };
-  hero: {
-    headingGradient: string;
-    leftBg: string;
-    rightBg: string;
-  };
-  companyStats: {
-    bg: string;
-    shadow: string;
-    border: string;
-  };
-};
-
-type Colors = { [K in ColorScheme]: ColorVariant } & AdditionalColors;
-
 export type HeadingElement = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
 
 export type TextElement =
@@ -110,9 +39,4 @@ export type WrapperElement =
 
 export type ThemeType = typeof theme;
 
-declare module 'styled-components' {
-  export interface DefaultTheme extends Omit<ThemeType, 'colors'> {
-    colors?: Colors;
-    isLight?: boolean;
-  }
-}
+declare module 'styled-components' {}
