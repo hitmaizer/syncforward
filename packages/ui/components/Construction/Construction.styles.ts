850a198f3e872283d4bd657aff87b6652fd17ab4
Jose Alves <jose.sousalves@gmail.com>
2022-06-24 17:57:39 +0100

    feat(Construction): adds sc to every icon + hover fxs

--- packages/ui/components/Construction/Construction.styles.ts
+++ packages/ui/components/Construction/Construction.styles.ts
@@ -1,7 +1,58 @@
 import styled from 'styled-components';
 
+import {
+  SfAppleMusic,
+  SfBeatport,
+  SfSoundcloud,
+  SfBandcamp,
+  SfSpotify,
+  SfYoutube,
+} from '@icons';
+
 export const Construction = styled.div`
   width: 100vw;
   height: 100vh;
   background-color: ${({ theme }) => theme.colors.bg};
+`;
+
+export const BandcampIcon = styled(SfBandcamp)`
+  transition: color 300ms ease;
+  &:hover {
+    color: ${({ theme }) => theme.colors.white};
+  }
+`;
+
+export const SpotifyIcon = styled(SfSpotify)`
+  transition: color 300ms ease;
+  &:hover {
+    color: ${({ theme }) => theme.colors.white};
+  }
+`;
+
+export const BeatportIcon = styled(SfBeatport)`
+  transition: color 300ms ease;
+  &:hover {
+    color: ${({ theme }) => theme.colors.white};
+  }
+`;
+
+export const ApplemusicIcon = styled(SfAppleMusic)`
+  transition: color 300ms ease;
+  &:hover {
+    color: ${({ theme }) => theme.colors.white};
+  }
+`;
+
+export const SoundcloudIcon = styled(SfSoundcloud)`
+  transition: color 300ms ease;
+  &:hover {
+    color: ${({ theme }) => theme.colors.white};
+  }
+`;
+
+export const YoutubeIcon = styled(SfYoutube)`
+  transition: color 300ms ease;
+  &:hover {
+    color: ${({ theme }) => theme.colors.white};
+  }
 `;
