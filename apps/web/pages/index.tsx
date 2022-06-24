09f41da4f8d4a143517217e9c37c5f7714a92651
Jose Alves <jose.sousalves@gmail.com>
2022-06-24 17:00:13 +0100

    feat(homepage): adds random sentence on homepage render

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,10 +1,18 @@
-import { SfSync } from '@icons';
+import { useEffect, useState } from 'react';
+
+import { phrases } from 'src/uConstruction';
+
+import { Construction } from '@uicomponents';
 
 export default function Home() {
+  const [sentance, setSentance] = useState<string>('');
+  useEffect(() => {
+    setSentance(phrases[Math.floor(Math.random() * phrases.length)]);
+  }, []);
+
   return (
     <>
-      <h1>hello mate!</h1>
-      <SfSync size="md" color="black" />
+      <Construction text={sentance} />
     </>
   );
 }
