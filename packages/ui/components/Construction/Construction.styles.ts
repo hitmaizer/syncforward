ce2a48bb945eefb14a6bd4d2128046c3dab3fa9d
Jose Alves <jose.sousalves@gmail.com>
2022-06-25 22:31:14 +0100

    style(Construction ): adds Stores component + responsiveness rules

--- packages/ui/components/Construction/Construction.styles.ts
+++ packages/ui/components/Construction/Construction.styles.ts
@@ -1,4 +1,7 @@
 import styled from 'styled-components';
+import { flexbox, gridGap, layout, space } from 'styled-system';
+
+import { mediaQueries } from '@styles';
 
 import {
   AppleMusic,
@@ -9,64 +12,99 @@ import {
   Sync,
   Youtube,
 } from '../../logos/Logos';
+import { ConstructionProps } from './Construction.types';
 
 export const Construction = styled.div`
   width: 100vw;
   height: 100vh;
   background-color: ${({ theme }) => theme.colors.bg};
+  padding: 16px;
+`;
+
+export const Stores = styled.div<ConstructionProps>`
+  ${layout}
+  ${flexbox}
+  ${space}
+  ${gridGap}
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  gap: 32px;
+  ${mediaQueries.lg} {
+    flex-direction: row;
+    gap: 64px;
+  }
 `;
 
 export const BandcampLogo = styled(Bandcamp)`
-  transition: color 300ms ease;
-  color: ${({ theme }) => theme.colors.gray};
-  width: 200px;
-  &:hover {
-    color: ${({ theme }) => theme.colors.white};
-  }
-`;
-
-export const SpotifyLogo = styled(Spotify)`
-  transition: color 300ms ease;
-  color: ${({ theme }) => theme.colors.gray};
-  width: 150px;
-  &:hover {
-    color: ${({ theme }) => theme.colors.white};
-  }
-`;
-
-export const BeatportLogo = styled(Beatport)`
-  transition: color 300ms ease;
-  color: ${({ theme }) => theme.colors.gray};
-  width: 170px;
-  &:hover {
-    color: ${({ theme }) => theme.colors.white};
-  }
-`;
-
-export const ApplemusicLogo = styled(AppleMusic)`
-  transition: color 100ms ease;
-  color: ${({ theme }) => theme.colors.gray};
-  width: 150px;
-  &:hover {
-    color: ${({ theme }) => theme.colors.white};
-  }
-`;
-
-export const SoundcloudLogo = styled(Soundcloud)`
   transition: color 300ms ease;
   color: ${({ theme }) => theme.colors.gray};
   width: 100px;
   &:hover {
     color: ${({ theme }) => theme.colors.white};
   }
+  ${mediaQueries.lg} {
+    width: 200px;
+  }
+`;
+
+export const SpotifyLogo = styled(Spotify)`
+  transition: color 300ms ease;
+  color: ${({ theme }) => theme.colors.gray};
+  width: 100px;
+  &:hover {
+    color: ${({ theme }) => theme.colors.white};
+  }
+  ${mediaQueries.lg} {
+    width: 150px;
+  }
+`;
+
+export const BeatportLogo = styled(Beatport)`
+  transition: color 300ms ease;
+  color: ${({ theme }) => theme.colors.gray};
+  width: 100px;
+  &:hover {
+    color: ${({ theme }) => theme.colors.white};
+  }
+  ${mediaQueries.lg} {
+    width: 150px;
+  }
+`;
+
+export const ApplemusicLogo = styled(AppleMusic)`
+  transition: color 100ms ease;
+  color: ${({ theme }) => theme.colors.gray};
+  width: 100px;
+  &:hover {
+    color: ${({ theme }) => theme.colors.white};
+  }
+  ${mediaQueries.lg} {
+    width: 150px;
+  }
+`;
+
+export const SoundcloudLogo = styled(Soundcloud)`
+  transition: color 300ms ease;
+  color: ${({ theme }) => theme.colors.gray};
+  width: 75px;
+  &:hover {
+    color: ${({ theme }) => theme.colors.white};
+  }
+  ${mediaQueries.lg} {
+    width: 100px;
+  }
 `;
 
 export const YoutubeLogo = styled(Youtube)`
   transition: color 300ms ease;
   color: ${({ theme }) => theme.colors.gray};
-  width: 150px;
+  width: 100px;
   &:hover {
     color: ${({ theme }) => theme.colors.white};
+  }
+  ${mediaQueries.lg} {
+    width: 150px;
   }
 `;
 
