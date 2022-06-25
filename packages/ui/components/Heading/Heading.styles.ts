41ef2a9c7b0ea773b6e79dcb50bd1914b4f5f170
Jose Alves <jose.sousalves@gmail.com>
2022-06-25 22:32:30 +0100

    feat(Heading): adds textAlign styled-system props to Heading

--- packages/ui/components/Heading/Heading.styles.ts
+++ packages/ui/components/Heading/Heading.styles.ts
@@ -1,11 +1,11 @@
 import styled from 'styled-components';
-import { color, space, typography } from 'styled-system';
+import { color, space, textAlign, typography } from 'styled-system';
 
 import { size } from './Heading.variants';
 
 export const Heading = styled.h1`
   line-height: ${({ theme }) => theme.lineHeights.heading};
-
+  ${textAlign}
   ${typography}
   ${space}
   ${size}
--- packages/ui/components/Heading/Heading.types.ts
+++ packages/ui/components/Heading/Heading.types.ts
@@ -5,11 +5,16 @@ import {
   SpaceProps,
   ResponsiveValue,
   ColorProps,
+  TextAlignProps,
 } from 'styled-system';
 
 import { HeadingElement, TextElement } from '@styles';
 
-export interface HeadingProps extends LayoutProps, SpaceProps, ColorProps {
+export interface HeadingProps
+  extends LayoutProps,
+    SpaceProps,
+    ColorProps,
+    TextAlignProps {
   as?: TextElement | HeadingElement;
   size?: ResponsiveValue<'lg' | 'xl' | '2xl' | '3xl' | '4xl'>;
   color?: string;
