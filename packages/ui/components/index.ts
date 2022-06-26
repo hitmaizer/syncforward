d0b6842c89e6c308758b43d3e9e4fae4e88ea94c
Jose Alves <jose.sousalves@gmail.com>
2022-06-26 20:33:53 +0100

    feat(ui): updates components index exports file

--- packages/ui/components/index.ts
+++ packages/ui/components/index.ts
@@ -5,4 +5,5 @@ export { default as Construction } from './Construction';
 export { default as Grid } from './Grid';
 export { default as Spinner } from './Spinner';
 export { default as Stack } from './Stack';
+export { default as Stores } from './Stores';
 export { default as MusicCard } from './MusicCard';
