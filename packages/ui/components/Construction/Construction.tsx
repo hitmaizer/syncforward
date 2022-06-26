0fe76a689d3d0e4a4b3d962e67f63b517a38c960
Jose Alves <jose.sousalves@gmail.com>
2022-06-26 18:10:25 +0100

    feat(Construction): adds loading state + timeout for spinner

--- packages/ui/components/Construction/Construction.tsx
+++ packages/ui/components/Construction/Construction.tsx
@@ -1,73 +1,85 @@
+import { useEffect, useState } from 'react';
+
+import Spinner from '@uicomponents/Spinner';
+
 import Heading from '../Heading';
-import Stack from '../Stack';
 import * as S from './Construction.styles';
 import { ConstructionProps } from './Construction.types';
 
 const Construction = ({ children, text, ...rest }: ConstructionProps) => {
-  return (
-    <S.Construction {...rest}>
-      <Stack
-        display="flex"
-        vertical
-        gridGap="32px"
-        alignItems="center"
-        justifyContent="center"
-        height="100%"
-      >
-        <a href="/">
-          <S.SyncLogo />
-        </a>
+  const [loading, setLoading] = useState<boolean>(true);
 
-        <Heading color="white" size="4xl" textAlign="center">
-          {text}
-        </Heading>
-        <S.Stores display="flex" mt="16px">
-          <a
-            href="https://syncforward.bandcamp.com/"
-            target="_blank"
-            rel="noopener noreferrer nofollow"
-          >
-            <S.BandcampLogo />
-          </a>
-          <a
-            href="https://open.spotify.com/playlist/3OdzGZH6odwnQ47S7aHLZC?si=xh_emCWLT7aTdQ5N_7xyxQ&utm_source=copy-link"
-            target="_blank"
-            rel="noopener noreferrer nofollow"
-          >
-            <S.SpotifyLogo />
-          </a>
-          <a
-            href="https://www.beatport.com/label/sync-forward/32440"
-            target="_blank"
-            rel="noopener noreferrer nofollow"
-          >
-            <S.BeatportLogo />
-          </a>
-          <a
-            href="https://music.apple.com/us/album/backlash-single/1603469909"
-            target="_blank"
-            rel="noopener noreferrer nofollow"
-          >
-            <S.ApplemusicLogo />
-          </a>
-          <a
-            href="https://soundcloud.com/syncforward"
-            target="_blank"
-            rel="noopener noreferrer nofollow"
-          >
-            <S.SoundcloudLogo />
-          </a>
-          <a
-            href="https://www.youtube.com/c/SyncForward"
-            target="_blank"
-            rel="noopener noreferrer nofollow"
-          >
-            <S.YoutubeLogo />
-          </a>
-        </S.Stores>
-        {children}
-      </Stack>
-    </S.Construction>
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 1400);
+  }, []);
+
+  return (
+    <>
+      {loading && <Spinner />}
+      <S.Construction {...rest}>
+        <S.VideoBg loop muted autoPlay>
+          <source src="/neon-30.mp4" type="video/mp4" />
+        </S.VideoBg>
+        <S.Content display="flex" gridGap="32px" height="100%">
+          <S.Header display="flex" width="100%">
+            <a href="/">
+              <S.SyncLogo />
+            </a>
+            <Heading color="white" size="2xl" textAlign="right">
+              {text}
+            </Heading>
+          </S.Header>
+
+          <S.Stores display="flex">
+            <a
+              href="https://syncforward.bandcamp.com/"
+              target="_blank"
+              rel="noopener noreferrer nofollow"
+            >
+              <S.BandcampLogo />
+            </a>
+            <a
+              href="https://open.spotify.com/playlist/3OdzGZH6odwnQ47S7aHLZC?si=xh_emCWLT7aTdQ5N_7xyxQ&utm_source=copy-link"
+              target="_blank"
+              rel="noopener noreferrer nofollow"
+            >
+              <S.SpotifyLogo />
+            </a>
+            <a
+              href="https://www.beatport.com/label/sync-forward/32440"
+              target="_blank"
+              rel="noopener noreferrer nofollow"
+            >
+              <S.BeatportLogo />
+            </a>
+            <a
+              href="https://music.apple.com/us/album/backlash-single/1603469909"
+              target="_blank"
+              rel="noopener noreferrer nofollow"
+            >
+              <S.ApplemusicLogo />
+            </a>
+            <a
+              href="https://soundcloud.com/syncforward"
+              target="_blank"
+              rel="noopener noreferrer nofollow"
+            >
+              <S.SoundcloudLogo />
+            </a>
+            <a
+              href="https://www.youtube.com/c/SyncForward"
+              target="_blank"
+              rel="noopener noreferrer nofollow"
+            >
+              <S.YoutubeLogo />
+            </a>
+          </S.Stores>
+          {children}
+        </S.Content>
+      </S.Construction>
+    </>
   );
 };
 
