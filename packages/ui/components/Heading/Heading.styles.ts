0515f0983a901dd14dac0facd1bc517aa4c605ca
Jose Alves <jose.sousalves@gmail.com>
2022-06-26 20:36:02 +0100

    style(Heading): updates Heading component styles for onlyMob prop

--- packages/ui/components/Heading/Heading.styles.ts
+++ packages/ui/components/Heading/Heading.styles.ts
@@ -1,13 +1,25 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { color, space, textAlign, typography } from 'styled-system';
 
+import { mediaQueries } from '@styles';
+
+import { HeadingProps } from './Heading.types';
 import { size } from './Heading.variants';
 
-export const Heading = styled.h1`
+export const Heading = styled.h1<HeadingProps>`
   line-height: ${({ theme }) => theme.lineHeights.heading};
   ${textAlign}
   ${typography}
   ${space}
   ${size}
   ${color}
+
+  ${({ onlyMob }) =>
+    onlyMob &&
+    css`
+      display: block;
+      ${mediaQueries.lg} {
+        display: none;
+      }
+    `}
 `;
