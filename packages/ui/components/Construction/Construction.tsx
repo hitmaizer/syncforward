cacb56f15485ad8f1c58f77f7fa52ad928a692a6
Jose Alves <jose.sousalves@gmail.com>
2022-06-26 20:34:56 +0100

    refactor(Construction): updates Construction component markup

--- packages/ui/components/Construction/Construction.tsx
+++ packages/ui/components/Construction/Construction.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react';
 
+import Hamburger from '@uicomponents/Hamburger';
 import Spinner from '@uicomponents/Spinner';
+import Stores from '@uicomponents/Stores';
 
 import Heading from '../Heading';
 import * as S from './Construction.styles';
@@ -27,55 +29,23 @@ const Construction = ({ children, text, ...rest }: ConstructionProps) => {
             <a href="/">
               <S.SyncLogo />
             </a>
-            <Heading color="white" size="2xl" textAlign="right">
-              {text}
-            </Heading>
+            <Hamburger />
+            <S.HeadingContainer>
+              <Heading color="white" size="2xl" textAlign="right">
+                {text}
+              </Heading>
+            </S.HeadingContainer>
           </S.Header>
-
-          <S.Stores display="flex">
-            <a
-              href="https://syncforward.bandcamp.com/"
-              target="_blank"
-              rel="noopener noreferrer nofollow"
-            >
-              <S.BandcampLogo />
-            </a>
-            <a
-              href="https://open.spotify.com/playlist/3OdzGZH6odwnQ47S7aHLZC?si=xh_emCWLT7aTdQ5N_7xyxQ&utm_source=copy-link"
-              target="_blank"
-              rel="noopener noreferrer nofollow"
-            >
-              <S.SpotifyLogo />
-            </a>
-            <a
-              href="https://www.beatport.com/label/sync-forward/32440"
-              target="_blank"
-              rel="noopener noreferrer nofollow"
-            >
-              <S.BeatportLogo />
-            </a>
-            <a
-              href="https://music.apple.com/us/album/backlash-single/1603469909"
-              target="_blank"
-              rel="noopener noreferrer nofollow"
-            >
-              <S.ApplemusicLogo />
-            </a>
-            <a
-              href="https://soundcloud.com/syncforward"
-              target="_blank"
-              rel="noopener noreferrer nofollow"
-            >
-              <S.SoundcloudLogo />
-            </a>
-            <a
-              href="https://www.youtube.com/c/SyncForward"
-              target="_blank"
-              rel="noopener noreferrer nofollow"
-            >
-              <S.YoutubeLogo />
-            </a>
-          </S.Stores>
+          <Heading
+            color="white"
+            size="4xl"
+            textAlign="center"
+            mt="70vh"
+            onlyMob
+          >
+            {text}
+          </Heading>
+          <Stores mt="70vh" noMob />
           {children}
         </S.Content>
       </S.Construction>
