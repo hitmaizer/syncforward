a4311526a45ad441169bb3953e5830edfd0fe65d
Jose Alves <jose.sousalves@gmail.com>
2022-06-27 15:06:10 +0100

    style(Header): adds bottom-border on Header mob

--- packages/ui/components/Construction/Construction.styles.ts
+++ packages/ui/components/Construction/Construction.styles.ts
@@ -79,12 +79,14 @@ export const Header = styled.div<ConstructionProps>`
   z-index: 10;
   background-color: ${({ theme }) => theme.colors.bg};
   backdrop-filter: saturate(130%) blur(5px);
-  // border-bottom: 1px solid ${({ theme }) => theme.colors.gray};
+  border-bottom: 1px solid ${({ theme }) => theme.colors.white};
+  min-height: 100px;
   ${mediaQueries.lg} {
     background-color: transparent;
     border: none;
     backdrop-filter: none;
     padding: 48px;
+    border-bottom: none;
   }
 `;
 
