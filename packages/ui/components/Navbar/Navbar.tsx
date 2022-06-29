8c8bdcef7cb790aed05766f0b535eaa6e1b7326a
Jose Alves <jose.sousalves@gmail.com>
2022-06-29 17:29:23 +0100

    refactor(Navbar): adds hamburger on mobile version + isOpen state

--- packages/ui/components/Navbar/Navbar.tsx
+++ packages/ui/components/Navbar/Navbar.tsx
@@ -1,3 +1,6 @@
+import { useState } from 'react';
+
+import Hamburger from '@uicomponents/Hamburger';
 import NavLink from '@uicomponents/NavLink';
 import Stack from '@uicomponents/Stack';
 
@@ -5,16 +8,33 @@ import * as S from './Navbar.styles';
 import { NavbarProps } from './Navbar.types';
 
 const Navbar = ({ children, ...rest }: NavbarProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <S.Navbar display="flex" justifyContent="space-between" {...rest}>
       <S.NavLogo />
       {children}
-      <Stack display="flex" gridGap="48px">
+      <S.Links gridGap="48px" noMob>
         <NavLink text="Releases" />
         <NavLink text="Podcasts" />
         <NavLink text="Artists" />
         <NavLink text="Demos" />
-      </Stack>
+      </S.Links>
+      <Hamburger open={isOpen} onClick={() => setIsOpen(!isOpen)} web>
+        <Stack
+          display="flex"
+          vertical
+          alignItems="center"
+          justifyContent="center"
+          width="100%"
+          gridGap="32px"
+        >
+          <NavLink text="Releases" mobOnly />
+          <NavLink text="Podcasts" mobOnly />
+          <NavLink text="Artists" mobOnly />
+          <NavLink text="Demos" mobOnly />
+        </Stack>
+      </Hamburger>
     </S.Navbar>
   );
 };
