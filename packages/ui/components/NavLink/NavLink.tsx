6a4ab03e4ea00678c9392d0cc95f604f760af7f7
Jose Alves <jose.sousalves@gmail.com>
2022-06-29 17:30:42 +0100

    feat(NavLink): adds mobOnly and noMob props

--- packages/ui/components/NavLink/NavLink.tsx
+++ packages/ui/components/NavLink/NavLink.tsx
@@ -1,10 +1,12 @@
 import * as S from './NavLink.styles';
 import { NavLinkProps } from './NavLink.types';
 
-const NavLink = ({ children, text, ...rest }: NavLinkProps) => {
+const NavLink = ({ children, text, mobOnly, noMob, ...rest }: NavLinkProps) => {
   return (
-    <S.NavLink href={text.toLowerCase()} {...rest}>
-      <S.LinkText>{text}</S.LinkText>
+    <S.NavLink href={text?.toLowerCase()} {...rest}>
+      <S.LinkText mobOnly={mobOnly} noMob={noMob}>
+        {text}
+      </S.LinkText>
       {children}
     </S.NavLink>
   );
