1b1c1142d3ba4d80c4d2a8ac5f14778027ec0572
Jose Alves <jose.sousalves@gmail.com>
2022-06-30 16:17:59 +0100

    refactor(Hamburger): adds local state with current window.innerHeight

--- packages/ui/components/Hamburger/Hamburger.tsx
+++ packages/ui/components/Hamburger/Hamburger.tsx
@@ -1,14 +1,30 @@
+import { useEffect, useState } from 'react';
+
 import Stores from '@uicomponents/Stores';
 
 import * as S from './Hamburger.styles';
 import { HamburgerProps } from './Hamburger.types';
 
 const Hamburger = ({ children, open, ...rest }: HamburgerProps) => {
+  const [height, setHeight] = useState(0);
+
+  const updateDimensions = () => {
+    if (typeof window !== 'undefined') {
+      setHeight(window.innerHeight);
+    }
+  };
+
+  useEffect(() => {
+    updateDimensions();
+    window.addEventListener('resize', updateDimensions);
+    return () => window.removeEventListener('resize', updateDimensions);
+  }, []);
+
   return (
     <>
       <S.Container {...rest}>
         <S.Hamburger open={open} {...rest} />
-        <S.HamburgerContent open={open}>
+        <S.HamburgerContent open={open} height={height}>
           <Stores vertical />
         </S.HamburgerContent>
         {children}
