d865aaf5d20715abb5beb6b3ca3339b6f8600b78
Jose Alves <jose.sousalves@gmail.com>
2022-07-01 20:54:33 +0100

    refactor(NavLink): adds next/link + pathName props

--- packages/ui/components/NavLink/NavLink.tsx
+++ packages/ui/components/NavLink/NavLink.tsx
@@ -1,14 +1,27 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
 import * as S from './NavLink.styles';
 import { NavLinkProps } from './NavLink.types';
 
-const NavLink = ({ children, text, mobOnly, noMob, ...rest }: NavLinkProps) => {
+const NavLink = ({
+  children,
+  text,
+  mobOnly,
+  noMob,
+  pathName,
+  ...rest
+}: NavLinkProps) => {
+  const router = useRouter();
   return (
-    <S.NavLink href={text?.toLowerCase()} {...rest}>
-      <S.LinkText mobOnly={mobOnly} noMob={noMob}>
-        {text}
-      </S.LinkText>
-      {children}
-    </S.NavLink>
+    <Link href={`/${pathName}`} passHref>
+      <S.NavLink pathName={router.pathname} {...rest}>
+        <S.LinkText mobOnly={mobOnly} noMob={noMob}>
+          {text}
+        </S.LinkText>
+        {children}
+      </S.NavLink>
+    </Link>
   );
 };
 
