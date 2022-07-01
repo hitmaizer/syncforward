a81d34c3135286599bfd5f2e4bc1a78fa272c5e8
Jose Alves <jose.sousalves@gmail.com>
2022-07-01 21:02:33 +0100

    refactor(Head): updates dynamic favicon logic to Head component

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,4 +1,4 @@
-import { FC, useState, useEffect, useCallback } from 'react';
+import { FC, useState, useEffect } from 'react';
 
 import 'src/font-face.css';
 import Head from '@components/Head';
@@ -13,46 +13,19 @@ import { theme } from 'ui/styles/theme';
 
 const MyApp: FC<AppProps> = ({ Component, pageProps }) => {
   const [mounted, setMounted] = useState(false);
-  const [isDark, setIsDark] = useState<boolean>();
+
   const { asPath } = useRouter();
 
   const page = asPath === '/' ? '' : asPath;
   const [canonicalUrl] = `https://sync-forward.com${page}`.split('?');
 
-  const getDeviceScheme = useCallback(() => {
-    // Prevents SSR issues
-    if (typeof window === 'undefined') return 'dark';
-    return window.matchMedia('(prefers-color-scheme: dark)').matches
-      ? 'dark'
-      : 'light';
-  }, []);
-
-  if (typeof window !== 'undefined') {
-    window
-      .matchMedia('(prefers-color-scheme: dark)')
-      .addEventListener('change', (event) => {
-        setIsDark(event.matches);
-      });
-  }
-
-  const changeFavicon = (value: string) => {
-    const faviconTag = document.getElementById('faviconTag') as HTMLLinkElement;
-    faviconTag?.setAttribute('href', `/favicon-${value}.ico`);
-  };
-
   useEffect(() => {
     setMounted(true);
-    const value = getDeviceScheme();
-    changeFavicon(value);
-    if (value === 'dark') {
-      setIsDark(true);
-    } else {
-      setIsDark(false);
-    }
-  }, [isDark]);
+  }, []);
 
   return (
     <>
+      <Head />
       <ThemeProvider theme={theme}>
         <DefaultSeo
           canonical={canonicalUrl}
@@ -65,7 +38,6 @@ const MyApp: FC<AppProps> = ({ Component, pageProps }) => {
           {...SEO}
         />
         <GlobalStyle />
-        <Head />
         {mounted && <Component {...pageProps} />}
       </ThemeProvider>
     </>
--- apps/web/src/components/Head/Head.tsx
+++ apps/web/src/components/Head/Head.tsx
@@ -1,7 +1,42 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import { useCallback, useEffect, useState } from 'react';
+
 import Head from 'next/head';
 
 export default function Layout() {
+  const [isDark, setIsDark] = useState<boolean>();
+
+  const getDeviceScheme = useCallback(() => {
+    // Prevents SSR issues
+    if (typeof window === 'undefined') return 'dark';
+    return window.matchMedia('(prefers-color-scheme: dark)').matches
+      ? 'dark'
+      : 'light';
+  }, []);
+
+  if (typeof window !== 'undefined') {
+    window
+      .matchMedia('(prefers-color-scheme: dark)')
+      .addEventListener('change', (event) => {
+        setIsDark(event.matches);
+      });
+  }
+
+  const changeFavicon = (value: string) => {
+    const faviconTag = document.getElementById('faviconTag') as HTMLLinkElement;
+    faviconTag?.setAttribute('href', `/favicon-${value}.ico`);
+  };
+
+  useEffect(() => {
+    const value = getDeviceScheme();
+    changeFavicon(value);
+    if (value === 'dark') {
+      setIsDark(true);
+    } else {
+      setIsDark(false);
+    }
+  }, [isDark]);
+
   return (
     <>
       <Head>
