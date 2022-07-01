d4f3172b59271295e5ba0f57c2f77266ad651917
Jose Alves <jose.sousalves@gmail.com>
2022-07-01 20:54:07 +0100

    refactor(NavLink): adds active link styling

--- packages/ui/components/NavLink/NavLink.styles.ts
+++ packages/ui/components/NavLink/NavLink.styles.ts
@@ -3,9 +3,18 @@ import styled, { css } from 'styled-components';
 
 import { NavLinkProps } from './NavLink.types';
 
-export const NavLink = styled.a`
+export const NavLink = styled.a<NavLinkProps>`
   color: ${({ theme }) => theme.colors.gray75};
   width: 100%;
+  ${({ href, pathName }) =>
+    href === pathName &&
+    css`
+      h2 {
+        text-shadow: rgb(255 255 255 / 50%) 0 0 15px;
+        opacity: 1;
+        color: ${({ theme }) => theme.colors.gray50};
+      }
+    `};
 `;
 
 export const LinkText = styled(Heading)<NavLinkProps>`
