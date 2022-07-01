bb5c4ba3b31bc179894089c3b299ccdcce13db51
Jose Alves <jose.sousalves@gmail.com>
2022-07-01 20:55:10 +0100

    feat(Navbar): adds pathName prop to NavLink elements

--- packages/ui/components/Navbar/Navbar.tsx
+++ packages/ui/components/Navbar/Navbar.tsx
@@ -16,10 +16,10 @@ const Navbar = ({ children, ...rest }: NavbarProps) => {
       <S.NavLogo />
       {children}
       <S.Links gridGap="48px" noMob>
-        <NavLink text="Releases" />
-        <NavLink text="Podcasts" />
-        <NavLink text="Artists" />
-        <NavLink text="Demos" />
+        <NavLink text="Releases" pathName="releases" />
+        <NavLink text="Podcasts" pathName="podcasts" />
+        <NavLink text="Artists" pathName="artists" />
+        <NavLink text="Demos" pathName="demos" />
       </S.Links>
       <Hamburger open={isOpen} onClick={() => setIsOpen(!isOpen)} web>
         <Stack
@@ -38,10 +38,10 @@ const Navbar = ({ children, ...rest }: NavbarProps) => {
             gridGap="32px"
             flex={1}
           >
-            <NavLink text="Releases" mobOnly />
-            <NavLink text="Podcasts" mobOnly />
-            <NavLink text="Artists" mobOnly />
-            <NavLink text="Demos" mobOnly />
+            <NavLink text="Releases" pathName="releases" mobOnly />
+            <NavLink text="Podcasts" pathName="podcasts" mobOnly />
+            <NavLink text="Artists" pathName="artists" mobOnly />
+            <NavLink text="Demos" pathName="demos" mobOnly />
           </Stack>
           <Socials />
         </Stack>
