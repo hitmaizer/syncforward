d3e029cb7fee330b8cf85ec43e1370f4c0475b70
Jose Alves <jose.sousalves@gmail.com>
2022-07-01 20:58:15 +0100

    feat(web): adds releases page file

---
+++ apps/web/pages/releases/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Accordion, Navbar } from '@uicomponents';
+
+const index = () => {
+  return (
+    <>
+      <Navbar />
+      <Accordion />
+    </>
+  );
+};
+
+export default index;
