625e79fb622106fdd926762391e4f3e1803ecccc
Jose Alves <jose.sousalves@gmail.com>
2022-07-01 20:55:29 +0100

    feat(mockData): adds mockData to implement accordion

---
+++ apps/web/src/mockData.ts
@@ -0,0 +1,138 @@
+import { Release } from '@uicomponents/Accordion/Accordion.types';
+
+const mockReleases: Release[] = [
+  {
+    artists: 'Remotion & Yerom',
+    title: 'Backlash',
+    catalogue: 'SF042',
+    description:
+      'To mark the 56th release at Sync Forward we return with a banger two-track single by the Portuguese duo Remotion.',
+    trackLink:
+      'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1209898735&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true',
+    link: 'https://soundcloud.com/remotion-yerom/backlash-remotion-yerom',
+    img: 'https://res.cloudinary.com/sync-forward/image/upload/v1656669665/mock-bg2_udjlpg.png',
+  },
+  {
+    artists: 'ATÓM',
+    title: 'Geata',
+    catalogue: 'SF042',
+    description:
+      'To mark the 56th release at Sync Forward we return with a banger two-track single by the Portuguese duo Remotion.',
+    trackLink:
+      'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1209898735&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true',
+    link: 'https://soundcloud.com/remotion-yerom/backlash-remotion-yerom',
+    img: 'https://res.cloudinary.com/sync-forward/image/upload/v1656669665/mock-bg5_wkjjdt.png',
+  },
+  {
+    artists: 'Pain&Panic',
+    title: 'Strike Out',
+    catalogue: 'SF042',
+    description:
+      'To mark the 56th release at Sync Forward we return with a banger two-track single by the Portuguese duo Remotion.',
+    trackLink:
+      'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1209898735&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true',
+    link: 'https://soundcloud.com/remotion-yerom/backlash-remotion-yerom',
+    img: 'https://res.cloudinary.com/sync-forward/image/upload/v1656669665/mock-bg_thqk6f.png',
+  },
+  {
+    artists: 'Remotion ',
+    title: 'Breach',
+    catalogue: 'SF042',
+    description:
+      'To mark the 56th release at Sync Forward we return with a banger two-track single by the Portuguese duo Remotion.',
+    trackLink:
+      'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1209898735&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true',
+    link: 'https://soundcloud.com/remotion-yerom/backlash-remotion-yerom',
+    img: 'https://res.cloudinary.com/sync-forward/image/upload/v1656669665/mock-bg4_ycbcra.png',
+  },
+  {
+    artists: 'Remotion & Yerom',
+    title: 'Backlash',
+    catalogue: 'SF042',
+    description:
+      'To mark the 56th release at Sync Forward we return with a banger two-track single by the Portuguese duo Remotion.',
+    trackLink:
+      'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1209898735&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true',
+    link: 'https://soundcloud.com/remotion-yerom/backlash-remotion-yerom',
+    img: 'https://res.cloudinary.com/sync-forward/image/upload/v1656669665/mock-bg3_ghffh7.png',
+  },
+  {
+    artists: 'Remotion & Yerom',
+    title: 'Backlash',
+    catalogue: 'SF042',
+    description:
+      'To mark the 56th release at Sync Forward we return with a banger two-track single by the Portuguese duo Remotion.',
+    trackLink:
+      'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1209898735&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true',
+    link: 'https://soundcloud.com/remotion-yerom/backlash-remotion-yerom',
+    img: 'https://res.cloudinary.com/sync-forward/image/upload/v1656669665/mock-bg2_udjlpg.png',
+  },
+  {
+    artists: 'Remotion & Yerom',
+    title: 'Backlash',
+    catalogue: 'SF042',
+    description:
+      'To mark the 56th release at Sync Forward we return with a banger two-track single by the Portuguese duo Remotion.',
+    trackLink:
+      'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1209898735&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true',
+    link: 'https://soundcloud.com/remotion-yerom/backlash-remotion-yerom',
+    img: 'https://res.cloudinary.com/sync-forward/image/upload/v1656669665/mock-bg2_udjlpg.png',
+  },
+  {
+    artists: 'Remotion & Yerom',
+    title: 'Backlash',
+    catalogue: 'SF042',
+    description:
+      'To mark the 56th release at Sync Forward we return with a banger two-track single by the Portuguese duo Remotion.',
+    trackLink:
+      'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1209898735&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true',
+    link: 'https://soundcloud.com/remotion-yerom/backlash-remotion-yerom',
+    img: 'https://res.cloudinary.com/sync-forward/image/upload/v1656669665/mock-bg2_udjlpg.png',
+  },
+  {
+    artists: 'Remotion & Yerom',
+    title: 'Backlash',
+    catalogue: 'SF042',
+    description:
+      'To mark the 56th release at Sync Forward we return with a banger two-track single by the Portuguese duo Remotion.',
+    trackLink:
+      'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1209898735&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true',
+    link: 'https://soundcloud.com/remotion-yerom/backlash-remotion-yerom',
+    img: 'https://res.cloudinary.com/sync-forward/image/upload/v1656669665/mock-bg2_udjlpg.png',
+  },
+  {
+    artists: 'Remotion & Yerom',
+    title: 'Backlash',
+    catalogue: 'SF042',
+    description:
+      'To mark the 56th release at Sync Forward we return with a banger two-track single by the Portuguese duo Remotion.',
+    trackLink:
+      'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1209898735&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true',
+    link: 'https://soundcloud.com/remotion-yerom/backlash-remotion-yerom',
+    img: 'https://res.cloudinary.com/sync-forward/image/upload/v1656669665/mock-bg2_udjlpg.png',
+  },
+  {
+    artists: 'Remotion & Yerom',
+    title: 'Backlash',
+    catalogue: 'SF042',
+    description:
+      'To mark the 56th release at Sync Forward we return with a banger two-track single by the Portuguese duo Remotion.',
+    trackLink:
+      'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1209898735&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true',
+    link: 'https://soundcloud.com/remotion-yerom/backlash-remotion-yerom',
+    img: 'https://res.cloudinary.com/sync-forward/image/upload/v1656669665/mock-bg2_udjlpg.png',
+  },
+  {
+    artists: 'Remotion & Yerom',
+    title: 'Backlash',
+    catalogue: 'SF042',
+    description:
+      'To mark the 56th release at Sync Forward we return with a banger two-track single by the Portuguese duo Remotion.',
+    trackLink:
+      'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1209898735&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true',
+    link: 'https://soundcloud.com/remotion-yerom/backlash-remotion-yerom',
+    img: 'https://res.cloudinary.com/sync-forward/image/upload/v1656669665/mock-bg2_udjlpg.png',
+  },
+];
+
+export default mockReleases;
