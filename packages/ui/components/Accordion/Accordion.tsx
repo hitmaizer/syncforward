708f785d76cd9307840863c3afb8f5f263a1bd42
Jose Alves <jose.sousalves@gmail.com>
2022-07-12 17:36:44 +0100

    feat(Accordion): adds height local state logic + height prop

--- packages/ui/components/Accordion/Accordion.tsx
+++ packages/ui/components/Accordion/Accordion.tsx
@@ -1,8 +1,10 @@
 /* eslint-disable react/no-array-index-key */
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import { theme } from '@styles/theme';
+import Button from '@uicomponents/Button';
 import Heading from '@uicomponents/Heading';
+import Stack from '@uicomponents/Stack';
 import Text from '@uicomponents/Text';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
@@ -15,6 +17,19 @@ import 'swiper/swiper.min.css';
 
 const Accordion = ({ children, data = mockData, ...rest }: AccordionProps) => {
   const [activeAccordion, setActiveAccordion] = useState(-1);
+  const [height, setHeight] = useState(0);
+
+  const updateDimensions = () => {
+    if (typeof window !== 'undefined') {
+      setHeight(window.innerHeight);
+    }
+  };
+
+  useEffect(() => {
+    updateDimensions();
+    window.addEventListener('resize', updateDimensions);
+    return () => window.removeEventListener('resize', updateDimensions);
+  }, []);
 
   const toggleAccordion = (index) => {
     if (index === activeAccordion) {
@@ -56,16 +71,68 @@ const Accordion = ({ children, data = mockData, ...rest }: AccordionProps) => {
                     justifyContent="space-between"
                     padding="64px"
                     flexDirection="column"
+                    height={height}
                   >
-                    <Text color={theme.colors.gray75} textAlign="center">
-                      {item.artists.toUpperCase()}
-                    </Text>
-                    <Heading color={theme.colors.gray75} textAlign="center">
-                      {item.title}
-                    </Heading>
-                    <Text color={theme.colors.gray75} textAlign="center">
-                      {item.catalogue}
-                    </Text>
+                    {activeAccordion !== index && (
+                      <>
+                        <Text color={theme.colors.gray75} textAlign="center">
+                          {item.artists.toUpperCase()}
+                        </Text>
+                        <Heading color={theme.colors.gray75} textAlign="center">
+                          {item.title}
+                        </Heading>
+                        <Text color={theme.colors.gray75} textAlign="center">
+                          {item.catalogue}
+                        </Text>
+                      </>
+                    )}
+                    {activeAccordion === index && (
+                      <>
+                        <Stack
+                          display="flex"
+                          vertical
+                          gridGap="16px"
+                          maxWidth="715px"
+                          justifyContent="flex-start"
+                          alignItems="flex-start"
+                          height="max-content"
+                        >
+                          <Text
+                            textTransform="uppercase"
+                            color={theme.colors.gray75}
+                            fontWeight="bold"
+                          >
+                            available now or something
+                          </Text>
+                          <Heading size="4xl" color={theme.colors.gray75}>
+                            {item.artists}
+                          </Heading>
+                          <Heading size="4xl" color={theme.colors.gray75}>
+                            {item.title}
+                          </Heading>
+                          <Text color={theme.colors.gray100}>
+                            {item.description}
+                          </Text>
+                          <iframe
+                            title={item.title}
+                            width="100%"
+                            height="166"
+                            scrolling="no"
+                            frameBorder="no"
+                            allow="autoplay"
+                            src={item.trackLink}
+                          />
+                          <Stack display="flex" gridGap="16px">
+                            <Button>
+                              <Text>Stream/Download</Text>
+                            </Button>
+                            <Button secondary>
+                              <Text>More from this artist</Text>
+                            </Button>
+                          </Stack>
+                        </Stack>
+                      </>
+                    )}
                   </S.AccordionContent>
                 </S.Slide>
               </SwiperSlide>
