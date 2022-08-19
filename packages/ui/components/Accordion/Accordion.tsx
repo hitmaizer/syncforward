4e9bd7776136013cd90d753e7472a6200b3db049
Jose Alves <jose.sousalves@gmail.com>
2022-08-19 16:56:55 +0100

    refactor(Accordion): removes padding from content and adds ml

--- packages/ui/components/Accordion/Accordion.tsx
+++ packages/ui/components/Accordion/Accordion.tsx
@@ -70,7 +70,7 @@ const Accordion = ({ children, data = mockData, ...rest }: AccordionProps) => {
                     display="flex"
                     alignItems="center"
                     justifyContent="space-between"
-                    padding="32px"
+                    ml="32px"
                     flexDirection="column"
                     height="75vh"
                   >
