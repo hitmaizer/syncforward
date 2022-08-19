2712cc67c1cd852d84cdc3e0b7ef0c09224bb68b
Jose Alves <jose.sousalves@gmail.com>
2022-08-19 16:26:38 +0100

    feat(icons): updates icons index file

--- packages/ui/icons/index.tsx
+++ packages/ui/icons/index.tsx
@@ -1,29 +1,31 @@
 import { SpaceProps, ColorProps } from 'styled-system';
 
- import SfYoutube from './Youtube';
+import SfYoutube from './Youtube';
 import SfAppleMusic from './AppleMusic';
 import SfArrow from './Arrow';
+import SfBandcampSq from './BandcampSq';
 import SfBandcamp from './Bandcamp';
 import SfBeatport from './Beatport';
+import SfSoundcloudSq from './SoundcloudSq';
 import SfSoundcloud from './Soundcloud';
 import SfSpotify from './Spotify';
 import SfSync from './Sync';
-import SfYoutube from './Youtube';
 
-  interface BaseProps {
-    size?: 'xxs' | 'xs' | 'sm' | 'md' | 'lg';
-  }
+interface BaseProps {
+  size?: 'xxs' | 'xs' | 'sm' | 'md' | 'lg';
+}
 
-  export type IconProps = BaseProps & SpaceProps & ColorProps;
+export type IconProps = BaseProps & SpaceProps & ColorProps;
 
- export {
+export {
   SfYoutube,
   SfAppleMusic,
   SfArrow,
+  SfBandcampSq,
   SfBandcamp,
   SfBeatport,
+  SfSoundcloudSq,
   SfSoundcloud,
   SfSpotify,
   SfSync,
-  SfYoutube
- };
+};
