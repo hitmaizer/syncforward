c332e94f46eae0fc84270c6c279c51ceb93f1738
Jose Alves <jose.sousalves@gmail.com>
2022-08-19 12:37:33 +0100

    feat(web): adds mockdata for artists

---
+++ apps/web/src/mockDataArtists.ts
@@ -0,0 +1,139 @@
+const artists = [
+  {
+    artists: 'Remotion',
+    photo:
+      'https://res.cloudinary.com/sync-forward/image/upload/v1660906495/remotion_xwncuo.png',
+    bio: 'Remotion are an electronic music duo based in Porto, Portugal. They’ve started making music under this alias in 2012 focusing on the deep and melodic techno vibes, with an energetic twist and aiming straight to the dance floor.',
+    soundcloud: 'https://soundcloud.com/remotion-yerom/backlash-remotion-yerom',
+    trackLink:
+      'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1209898735&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true',
+  },
+  {
+    artists: 'Dolbytall',
+    photo:
+      'https://res.cloudinary.com/sync-forward/image/upload/v1660906495/dolbytall_gr74gs.png',
+    bio: 'Remotion are an electronic music duo based in Porto, Portugal. They’ve started making music under this alias in 2012 focusing on the deep and melodic techno vibes, with an energetic twist and aiming straight to the dance floor.',
+    soundcloud: 'https://soundcloud.com/remotion-yerom/backlash-remotion-yerom',
+    trackLink:
+      'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1209898735&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true',
+  },
+  {
+    artists: 'Timo Veranta',
+    photo:
+      'https://res.cloudinary.com/sync-forward/image/upload/v1660906495/timo-veranta_pgk9m9.png',
+    bio: 'Remotion are an electronic music duo based in Porto, Portugal. They’ve started making music under this alias in 2012 focusing on the deep and melodic techno vibes, with an energetic twist and aiming straight to the dance floor.',
+    soundcloud: 'https://soundcloud.com/remotion-yerom/backlash-remotion-yerom',
+    trackLink:
+      'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1209898735&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true',
+  },
+  {
+    artists: 'Remotion',
+    photo:
+      'https://res.cloudinary.com/sync-forward/image/upload/v1660906495/remotion_xwncuo.png',
+    bio: 'Remotion are an electronic music duo based in Porto, Portugal. They’ve started making music under this alias in 2012 focusing on the deep and melodic techno vibes, with an energetic twist and aiming straight to the dance floor.',
+    soundcloud: 'https://soundcloud.com/remotion-yerom/backlash-remotion-yerom',
+    trackLink:
+      'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1209898735&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true',
+  },
+  {
+    artists: 'Remotion',
+    photo:
+      'https://res.cloudinary.com/sync-forward/image/upload/v1660906495/remotion_xwncuo.png',
+    bio: 'Remotion are an electronic music duo based in Porto, Portugal. They’ve started making music under this alias in 2012 focusing on the deep and melodic techno vibes, with an energetic twist and aiming straight to the dance floor.',
+    soundcloud: 'https://soundcloud.com/remotion-yerom/backlash-remotion-yerom',
+    trackLink:
+      'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1209898735&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true',
+  },
+  {
+    artists: 'Remotion',
+    photo:
+      'https://res.cloudinary.com/sync-forward/image/upload/v1660906495/remotion_xwncuo.png',
+    bio: 'Remotion are an electronic music duo based in Porto, Portugal. They’ve started making music under this alias in 2012 focusing on the deep and melodic techno vibes, with an energetic twist and aiming straight to the dance floor.',
+    soundcloud: 'https://soundcloud.com/remotion-yerom/backlash-remotion-yerom',
+    trackLink:
+      'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1209898735&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true',
+  },
+  {
+    artists: 'Remotion',
+    photo:
+      'https://res.cloudinary.com/sync-forward/image/upload/v1660906495/remotion_xwncuo.png',
+    bio: 'Remotion are an electronic music duo based in Porto, Portugal. They’ve started making music under this alias in 2012 focusing on the deep and melodic techno vibes, with an energetic twist and aiming straight to the dance floor.',
+    soundcloud: 'https://soundcloud.com/remotion-yerom/backlash-remotion-yerom',
+    trackLink:
+      'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1209898735&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true',
+  },
+  {
+    artists: 'Remotion',
+    photo:
+      'https://res.cloudinary.com/sync-forward/image/upload/v1660906495/remotion_xwncuo.png',
+    bio: 'Remotion are an electronic music duo based in Porto, Portugal. They’ve started making music under this alias in 2012 focusing on the deep and melodic techno vibes, with an energetic twist and aiming straight to the dance floor.',
+    soundcloud: 'https://soundcloud.com/remotion-yerom/backlash-remotion-yerom',
+    trackLink:
+      'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1209898735&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true',
+  },
+  {
+    artists: 'Remotion',
+    photo:
+      'https://res.cloudinary.com/sync-forward/image/upload/v1660906495/remotion_xwncuo.png',
+    bio: 'Remotion are an electronic music duo based in Porto, Portugal. They’ve started making music under this alias in 2012 focusing on the deep and melodic techno vibes, with an energetic twist and aiming straight to the dance floor.',
+    soundcloud: 'https://soundcloud.com/remotion-yerom/backlash-remotion-yerom',
+    trackLink:
+      'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1209898735&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true',
+  },
+  {
+    artists: 'Remotion',
+    photo:
+      'https://res.cloudinary.com/sync-forward/image/upload/v1660906495/remotion_xwncuo.png',
+    bio: 'Remotion are an electronic music duo based in Porto, Portugal. They’ve started making music under this alias in 2012 focusing on the deep and melodic techno vibes, with an energetic twist and aiming straight to the dance floor.',
+    soundcloud: 'https://soundcloud.com/remotion-yerom/backlash-remotion-yerom',
+    trackLink:
+      'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1209898735&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true',
+  },
+  {
+    artists: 'Remotion',
+    photo:
+      'https://res.cloudinary.com/sync-forward/image/upload/v1660906495/remotion_xwncuo.png',
+    bio: 'Remotion are an electronic music duo based in Porto, Portugal. They’ve started making music under this alias in 2012 focusing on the deep and melodic techno vibes, with an energetic twist and aiming straight to the dance floor.',
+    soundcloud: 'https://soundcloud.com/remotion-yerom/backlash-remotion-yerom',
+    trackLink:
+      'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1209898735&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true',
+  },
+  {
+    artists: 'Remotion',
+    photo:
+      'https://res.cloudinary.com/sync-forward/image/upload/v1660906495/remotion_xwncuo.png',
+    bio: 'Remotion are an electronic music duo based in Porto, Portugal. They’ve started making music under this alias in 2012 focusing on the deep and melodic techno vibes, with an energetic twist and aiming straight to the dance floor.',
+    soundcloud: 'https://soundcloud.com/remotion-yerom/backlash-remotion-yerom',
+    trackLink:
+      'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1209898735&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true',
+  },
+  {
+    artists: 'Remotion',
+    photo:
+      'https://res.cloudinary.com/sync-forward/image/upload/v1660906495/remotion_xwncuo.png',
+    bio: 'Remotion are an electronic music duo based in Porto, Portugal. They’ve started making music under this alias in 2012 focusing on the deep and melodic techno vibes, with an energetic twist and aiming straight to the dance floor.',
+    soundcloud: 'https://soundcloud.com/remotion-yerom/backlash-remotion-yerom',
+    trackLink:
+      'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1209898735&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true',
+  },
+  {
+    artists: 'Remotion',
+    photo:
+      'https://res.cloudinary.com/sync-forward/image/upload/v1660906495/remotion_xwncuo.png',
+    bio: 'Remotion are an electronic music duo based in Porto, Portugal. They’ve started making music under this alias in 2012 focusing on the deep and melodic techno vibes, with an energetic twist and aiming straight to the dance floor.',
+    soundcloud: 'https://soundcloud.com/remotion-yerom/backlash-remotion-yerom',
+    trackLink:
+      'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1209898735&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true',
+  },
+  {
+    artists: 'Remotion',
+    photo:
+      'https://res.cloudinary.com/sync-forward/image/upload/v1660906495/remotion_xwncuo.png',
+    bio: 'Remotion are an electronic music duo based in Porto, Portugal. They’ve started making music under this alias in 2012 focusing on the deep and melodic techno vibes, with an energetic twist and aiming straight to the dance floor.',
+    soundcloud: 'https://soundcloud.com/remotion-yerom/backlash-remotion-yerom',
+    trackLink:
+      'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1209898735&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true',
+  },
+];
+
+export default artists;
