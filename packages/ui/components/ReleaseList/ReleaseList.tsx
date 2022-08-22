cbe7bdfa97c3af13fa337339ff3b72eef346f12b
Jose Alves <jose.sousalves@gmail.com>
2022-08-22 16:22:42 +0100

    feat(ReleaseList): adds handler prop to pass it down

--- packages/ui/components/ReleaseList/ReleaseList.tsx
+++ packages/ui/components/ReleaseList/ReleaseList.tsx
@@ -3,14 +3,19 @@ import ReleaseCard from '@uicomponents/ReleaseCard';
 import * as S from './ReleaseList.styles';
 import { ReleaseListProps } from './ReleaseList.types';
 
-const ReleaseList = ({ children, data, ...rest }: ReleaseListProps) => {
+const ReleaseList = ({
+  children,
+  data,
+  changeBanner,
+  ...rest
+}: ReleaseListProps) => {
   return (
     <>
       <S.ReleaseList {...rest}>
         {data?.map((item) => {
           return (
             <>
-              <ReleaseCard data={item} />
+              <ReleaseCard data={item} changeBanner={changeBanner} />
             </>
           );
         })}
