d50901167d83f8089711d19839bb7d4fbfaf0b10
Jose Alves <jose.sousalves@gmail.com>
2022-08-22 16:21:48 +0100

    refactor(ReleaseCard): updates markup to match design file

--- packages/ui/components/ReleaseCard/ReleaseCard.tsx
+++ packages/ui/components/ReleaseCard/ReleaseCard.tsx
@@ -1,18 +1,36 @@
 import { theme } from '@styles/theme';
 import Stack from '@uicomponents/Stack';
+import Image from 'next/image';
 
 import * as S from './ReleaseCard.styles';
 import { ReleaseCardProps } from './ReleaseCard.types';
 
-const ReleaseCard = ({ children, data, ...rest }: ReleaseCardProps) => {
+const ReleaseCard = ({
+  children,
+  data,
+  changeBanner,
+  ...rest
+}: ReleaseCardProps) => {
   return (
     <>
-      <S.ReleaseCard {...rest}>
-        <Stack display="flex" position="relative">
-          <S.SImage src={data?.thumbnail!} width="100px" height="100px" />
-          <Stack display="flex" vertical>
-            <S.CardText color={theme.colors.gray75}>{data?.title}</S.CardText>
+      <S.ReleaseCard onClick={() => changeBanner(data!)} {...rest}>
+        <Stack
+          display="flex"
+          position="relative"
+          justifyContent="flex-start"
+          alignContent="center"
+          gridGap="24px"
+        >
+          <Image src={data?.thumbnail!} width="100px" height="100px" />
+          <Stack
+            display="flex"
+            vertical
+            placeItems="flex-start"
+            height="100%"
+            margin="auto 0"
+          >
             <S.CardText color={theme.colors.gray75}>{data?.artists}</S.CardText>
+            <S.CardText color={theme.colors.gray75}>{data?.title}</S.CardText>
           </Stack>
         </Stack>
         {children}
