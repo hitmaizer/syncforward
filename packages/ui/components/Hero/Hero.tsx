74deb4de5fa9d4e9bc65228b230e3933f8d33573
Jose Alves <jose.sousalves@gmail.com>
2022-08-22 15:47:55 +0100

    refactor(Hero): updates Hero component markup

--- packages/ui/components/Hero/Hero.tsx
+++ packages/ui/components/Hero/Hero.tsx
@@ -1,3 +1,9 @@
+import Banner from '@uicomponents/Banner';
+import ReleaseList from '@uicomponents/ReleaseList';
+import Stack from '@uicomponents/Stack';
+
+// placeholder data
+import data from '../../../../apps/web/src/mockData';
 import { useHeight } from '../../hooks/useHeight';
 import * as S from './Hero.styles';
 import { HeroProps } from './Hero.types';
@@ -6,7 +12,13 @@ const Hero = ({ children, ...rest }: HeroProps) => {
   const currentHeight = useHeight();
   return (
     <S.Hero {...rest} height={currentHeight}>
-      {children}
+      <Stack display="flex">
+        <Banner data={data[0]} />
+        <Stack display="flex" vertical>
+          <ReleaseList data={data} />
+        </Stack>
+        {children}
+      </Stack>
     </S.Hero>
   );
 };
