6dcae51a7248af770872530dc1c763b7db560fef
Jose Alves <jose.sousalves@gmail.com>
2022-09-18 12:56:08 +0100

    refactor(MusicCard): update component files to clean up code

--- packages/ui/components/MusicCard/MusicCard.tsx
+++ packages/ui/components/MusicCard/MusicCard.tsx
@@ -1,12 +1,10 @@
 import { useState } from 'react';
 
-import Image from 'next/image';
-
 import * as S from './MusicCard.styles';
 import { MusicCardProps } from './MusicCard.types';
 
 const MusicCard = ({
-  imgSrc,
+  imgSrc = '',
   soundcloudLink,
   title,
   iconSrc,
@@ -14,27 +12,20 @@ const MusicCard = ({
   const [showPlay, setShowPlay] = useState(true);
 
   const handlePlay = () => {
-    setShowPlay(!showPlay);
+    setShowPlay((state) => !state);
   };
 
   return (
     <>
-      <S.Card onClick={() => handlePlay()} showPlay={showPlay}>
+      <S.Card onClick={() => handlePlay()}>
         {!showPlay && (
           <S.SoundCloudContainer showPlay={showPlay}>
-            <iframe
-              title={title}
-              width="100%"
-              height="100%"
-              scrolling="no"
-              frameBorder="no"
-              allow="autoplay"
-              src={soundcloudLink}
-            />
+            <S.IFrame title={title} src={soundcloudLink} />
           </S.SoundCloudContainer>
         )}
-
-        <Image src={imgSrc!} alt="card-img" height="500px" width="500px" />
+        <S.ImageContainer>
+          <S.Image src={imgSrc} />
+        </S.ImageContainer>
         <S.OpenSoundCloud>
           <S.Icon src={iconSrc} alt="" />
         </S.OpenSoundCloud>
@@ -45,9 +36,4 @@ const MusicCard = ({
 
 export default MusicCard;
 
-MusicCard.defaultProps = {
-  imgSrc: '/assets/imgs/placeholder.jpg',
-  soundcloudLink:
-    'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1209898735&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true',
-  iconSrc: 'assets/imgs/bx-play-circle.svg',
-};
+MusicCard.defaultProps = {};
