36a71fb527a56434784cbcabbfe280a6597c5fa2
Jose Alves <jose.sousalves@gmail.com>
2022-09-25 11:23:38 +0100

    chore(constants): add social links on a constant obj file

---
+++ packages/ui/constants/socials.ts
@@ -0,0 +1,8 @@
+export const socialsLinks = {
+  facebook: 'https://www.facebook.com/SyncForward/',
+  twitter: 'https://twitter.com/sync_forward',
+  instagram: 'https://www.instagram.com/syncforward/',
+  youtube: 'https://www.youtube.com/user/syncforward',
+  bandcamp: 'https://syncforward.bandcamp.com/',
+  soundcloud: 'https://soundcloud.com/syncforward',
+};
